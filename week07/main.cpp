c8ca06e34a4062f2f0fe2443494bc3dbbdd7dbb6
ann1219 <80370362+ann1219@users.noreply.github.com>
2021-04-07 11:54:54 +0800

    Add files via upload

---
+++ week07/main.cpp
@@ -0,0 +1,39 @@
+#include <opencv/highgui.h> ///使用 OpenCV 2.1 比較簡單, 只要用 High GUI 即可
+#include <opencv/cv.h>
+#include <GL/glut.h>
+void init()
+{
+    IplImage * img = cvLoadImage("wink.jpg");
+    cvCvtColor(img,img, CV_BGR2RGB);
+    glEnable(GL_TEXTURE_2D);
+    GLuint id;
+    glGenTextures(1, &id);
+    glBindTexture(GL_TEXTURE_2D, id);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img->width, img->height, 0, GL_RGB, GL_UNSIGNED_BYTE, img->imageData);
+}
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glBegin(GL_POLYGON);
+        glTexCoord2f(0,1); glVertex2f(-1,-1);
+        glTexCoord2f(1,1); glVertex2f(+1,-1);
+        glTexCoord2f(1,0); glVertex2f(+1,+1);
+        glTexCoord2f(0,0); glVertex2f(-1,+1);
+    glEnd();
+    glutSolidTeapot(0.3);
+    glutSwapBuffers();
+}
+
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("08160661");
+    glutDisplayFunc(display);
+    init();
+    glutMainLoop();
+}
