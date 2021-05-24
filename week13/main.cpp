c10082dd157c24be1a549f83d01186bc71b6d15b
ann1219 <80370362+ann1219@users.noreply.github.com>
2021-05-24 16:30:53 +0800

    Add files via upload

---
+++ week13/main.cpp
@@ -0,0 +1,70 @@
+#include <opencv/highgui.h>
+#include <opencv/cv.h>
+#include <GL/glut.h>
+#include "glm.h"
+GLMmodel * pmodel = NULL;
+GLMmodel * arm1 = NULL;
+GLMmodel * arm2 = NULL;
+GLMmodel * hand = NULL;
+
+void
+drawmodel(void)
+{
+    if (!pmodel) {
+	pmodel = glmReadOBJ("Gundam.obj");
+	if (!pmodel) exit(0);
+	glmUnitize(pmodel);
+	glmFacetNormals(pmodel);
+	glmVertexNormals(pmodel, 90.0);
+    }
+
+    glmDraw(pmodel, GLM_SMOOTH | GLM_TEXTURE);
+}
+
+GLuint id1, id2;
+int myTexture(char * filename)
+{
+    IplImage * img = cvLoadImage(filename);
+    cvCvtColor(img,img, CV_BGR2RGB);
+    glEnable(GL_TEXTURE_2D);
+    GLuint id;
+    glGenTextures(1, &id);
+    glBindTexture(GL_TEXTURE_2D, id);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img->width, img->height, 0, GL_RGB, GL_UNSIGNED_BYTE, img->imageData);
+    return id;
+}
+float angle=180;
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glBindTexture(GL_TEXTURE_2D, id1);
+    glPushMatrix();
+        glRotatef(angle,0,1,0);
+        drawmodel();
+    glPopMatrix();
+    glBindTexture(GL_TEXTURE_2D, id2);
+    glBegin(GL_POLYGON);
+        glTexCoord2f( 0, 0 ); glVertex2f( -1, -1 );
+        glTexCoord2f( 0, 1 ); glVertex2f( -1, +1 );
+        glTexCoord2f( 1, 1 ); glVertex2f( +1, +1 );
+        glTexCoord2f( 1, 0 ); glVertex2f( +1, -1 );
+    glEnd();
+    glutSwapBuffers();
+}
+int main(int argc, char** argv)
+{
+    glutInit( &argc, argv );
+    glutInitDisplayMode( GLUT_DOUBLE | GLUT_DEPTH );
+    glutCreateWindow(" Week08 ");
+    glutDisplayFunc(display);
+
+    id1 = myTexture("Diffuse.jpg");
+    id2 = myTexture("bg.jpg");
+    glEnable(GL_DEPTH_TEST);
+    glutMainLoop();
+}
